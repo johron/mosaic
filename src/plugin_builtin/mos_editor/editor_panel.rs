bf4115ea2ef29b8cd8e99743582e99757729eb41
Johan Rong <59508244+johron@users.noreply.github.com>
2026-02-13 10:41:30 +0100

    render text editor, but panels do not have input yet

--- src/plugin_builtin/mos_editor/editor_panel.rs
+++ src/plugin_builtin/mos_editor/editor_panel.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
 use ratatui::Frame;
-use ratatui::layout::Rect;
+use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::style::{Color, Modifier, Style};
+use ratatui::text::{Line, Span};
+use ratatui::widgets::Paragraph;
 use ropey::Rope;
 use crate::app::MosId;
 use crate::event::event::Event;
@@ -26,6 +29,7 @@ pub struct EditorPanel {
     pub rope: Rope,
     pub cursors: Vec<Cursor>,
     pub file_path: Option<PathBuf>,
+    pub scroll_offset: usize,
 }
 
 impl EditorPanel {
@@ -34,7 +38,37 @@ impl EditorPanel {
             rope: Rope::new(),
             cursors: vec![Cursor::new(0, 0, 0)],
             file_path: None,
+            scroll_offset: 0,
         }
+    }
+
+    fn highlight_line(&self, max_line: usize) -> Vec<Line<'static>> {
+        //if let Some(syntax) = &self.editor.syntax {
+        //    syntax.highlight(
+        //        scroll_offset,
+        //        max_line,
+        //        &rope,
+        //    )
+        //} else {
+        //    Self::no_highlight_line(scroll_offset, max_line, rope)
+        //}
+        self.no_highlight_line(max_line)
+    }
+
+    fn no_highlight_line(&self, max_line: usize) -> Vec<Line<'static>> {
+        let mut lines_spans: Vec<Line> = Vec::new();
+
+        for i in self.scroll_offset..max_line {
+            let rope_line = self.rope.line(i);
+            let text_line = rope_line.to_string();
+            let spans = vec![Span::raw(text_line)];
+            let mut line_spans = vec![Span::styled(format!("{:4} ", i), Style::default().fg(Color::Gray))]; // small gutter
+
+            line_spans.extend(spans);
+            lines_spans.push(Line::from(line_spans));
+        }
+
+        lines_spans
     }
 }
 
@@ -52,6 +86,40 @@ impl Panel for EditorPanel {
     }
 
     fn render(&self, frame: &mut Frame, area: Rect) {
-        frame.render_widget(ratatui::widgets::Paragraph::new("self.rope.to_string()"), area);
+        //frame.render_widget(ratatui::widgets::Paragraph::new("self.rope.to_string()"), area);
+
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(100)].as_ref())
+            .split(area);
+
+        let height = std::cmp::max(1, chunks[0].height as usize - 1);
+
+        let max_line = std::cmp::min(
+            self.rope.len_lines(),
+            self.scroll_offset.saturating_add(height),
+        );
+
+        let lines_spans: Vec<Line> = self.highlight_line(max_line);
+
+        // Have to think about how I can to the multiple editor panels later, block should be set from outside, not in editor panel
+        let paragraph = Paragraph::new(lines_spans);
+        //.bg(Color::Red);
+        //.block(block);
+
+        frame.render_widget(paragraph, chunks[0]);
+
+        for cursor in &self.cursors {
+            let x = chunks[0].x + 5 + cursor.column as u16; // 5 for gutter
+            let y = chunks[0].y + (cursor.line.saturating_sub(self.scroll_offset)) as u16;
+            frame.render_widget(
+                Paragraph::new("")
+                    .style(Style::default()
+                        .add_modifier(Modifier::REVERSED)
+                        .fg(Color::White)),
+                //.fg(if panel.active { Color::White } else { Color::DarkGray })),
+                Rect::new(x, y, 1, 1),
+            );
+        }
     }
 }
